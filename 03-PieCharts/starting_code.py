74dda0aa610d3846b2ec9fdc25a71a4e4bd13978
Phil Lembo <phil.lembo@gmail.com>
2019-09-12 17:46:37 -0400

    Update

--- 03-PieCharts/starting_code.py
+++ 03-PieCharts/starting_code.py
@@ -2,6 +2,11 @@ from matplotlib import pyplot as plt
 
 plt.style.use("fivethirtyeight")
 
+slices = [60,40, 30, 20]
+labels = ['Sixty', 'Forty', 'Extra1', 'Extra2']
+colors = ['#008fd5', '#fc4f30', '#e5ae37', '#6d904f']
+
+plt.pie(slices, labels=labels, colors=colors, wedgeprops={'edgecolor': 'black'})
 
 plt.title("My Awesome Pie Chart")
 plt.tight_layout()
