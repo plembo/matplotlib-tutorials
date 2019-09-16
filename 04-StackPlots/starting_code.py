a0e387e192bfe4b9871dd96f102f528c8e3fb5e1
Phil Lembo <phil.lembo@gmail.com>
2019-09-16 11:16:47 -0400

    Add labels and legend

--- 04-StackPlots/starting_code.py
+++ 04-StackPlots/starting_code.py
@@ -9,7 +9,11 @@ player1 = [1, 2, 3, 3, 4, 4, 4, 4, 5]
 player2 = [1, 1, 1, 1, 2, 2, 2, 3, 4]
 player3 = [1, 1, 1, 2, 2, 2, 3, 3, 3]
 
-plt.stackplot(minutes, player1, player2, player3)
+labels = ['player1','player2','player3']
+
+plt.stackplot(minutes, player1, player2, player3, labels=labels)
+
+plt.legend()
 
 plt.title("My Awesome Stack Plot")
 plt.tight_layout()
