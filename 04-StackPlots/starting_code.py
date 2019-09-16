f1c41d0a12792478ac2d3eed60cab5816c927dea
Phil Lembo <phil.lembo@gmail.com>
2019-09-16 11:13:55 -0400

    Convert pie into stack chart

--- 04-StackPlots/starting_code.py
+++ 04-StackPlots/starting_code.py
@@ -9,7 +9,7 @@ player1 = [1, 2, 3, 3, 4, 4, 4, 4, 5]
 player2 = [1, 1, 1, 1, 2, 2, 2, 3, 4]
 player3 = [1, 1, 1, 2, 2, 2, 3, 3, 3]
 
-plt.pie([1, 1, 1], labels=["Player 1", "Player2", "Player3"])
+plt.stackplot(minutes, player1, player2, player3)
 
 plt.title("My Awesome Stack Plot")
 plt.tight_layout()
