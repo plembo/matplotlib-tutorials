dcb47ccb72eda981e3fa85b6106cb4842deca441
Phil Lembo <phil.lembo@gmail.com>
2019-09-16 13:36:55 -0400

    Fill between py and all dev sals

--- 05-Fill_Betweens/starting_code.py
+++ 05-Fill_Betweens/starting_code.py
@@ -15,18 +15,18 @@ plt.plot(ages, py_salaries, label='Python')
 overall_median = 57287
 
 plt.fill_between(ages, py_salaries, dev_salaries,
-                 where=(py_salaries > overall_median),
+                 where=(py_salaries > dev_salaries),
                  interpolate=True, alpha=0.25)
 
-plt.fill_between(ages, py_salaries, overall_median,
-                 where=(py_salaries <= overall_median),
+plt.fill_between(ages, py_salaries, dev_salaries,
+                 where=(py_salaries <= dev_salaries),
                  interpolate=True, color='red', alpha=0.25)
 
 plt.legend()
 
-plt.title('Median Salary (USD) by Age')
+plt.title('Developer Salary (USD) by Age')
 plt.xlabel('Ages')
-plt.ylabel('Median Salary (USD)')
+plt.ylabel('Developer Salary (USD)')
 
 plt.tight_layout()
 
