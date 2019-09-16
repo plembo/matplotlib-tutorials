461c1b78958494b473c39f008e676f962fe257ed
Phil Lembo <phil.lembo@gmail.com>
2019-09-16 11:54:53 -0400

    Change var names; title; add labels

--- 04-StackPlots/starting_code.py
+++ 04-StackPlots/starting_code.py
@@ -3,20 +3,22 @@ from matplotlib import pyplot as plt
 plt.style.use("fivethirtyeight")
 
 
-minutes = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+days = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-player1 = [8, 6, 5, 5, 4, 2, 1, 1, 0]
-player2 = [0, 1, 2, 2, 2, 4, 4, 4, 4]
-player3 = [0, 1, 1, 1, 2, 2, 3, 3, 4]
+dev1 = [8, 6, 5, 5, 4, 2, 1, 1, 0]
+dev2 = [0, 1, 2, 2, 2, 4, 4, 4, 4]
+dev3 = [0, 1, 1, 1, 2, 2, 3, 3, 4]
 
-labels = ['player1','player2','player3']
+labels = ['dev1','dev2','dev3']
 colors = ['#6d904f','#fc4f30','#008fd5']
 
-plt.stackplot(minutes, player1, player2, player3, labels=labels, colors=colors)
+plt.stackplot(days, dev1, dev2, dev3, labels=labels, colors=colors)
 
 plt.legend(loc=(0.07, 0.05))
 
-plt.title("My Awesome Stack Plot")
+plt.title("Dev Project Handover")
+plt.xlabel("Days")
+plt.ylabel("Hours")
 plt.tight_layout()
 plt.show()
 
