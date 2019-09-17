9c8460731ab7398bdf67d762ab72b586b6563a2d
Phil Lembo <phil.lembo@gmail.com>
2019-09-17 19:08:44 -0400

    Read data from file for bitcoin prices

--- 08-TimeSeries/starting_code.py
+++ 08-TimeSeries/starting_code.py
@@ -5,33 +5,17 @@ from matplotlib import dates as mpl_dates
 
 plt.style.use('seaborn')
 
-dates = [
-    datetime(2019, 5, 24),
-    datetime(2019, 5, 25),
-    datetime(2019, 5, 26),
-    datetime(2019, 5, 27),
-    datetime(2019, 5, 28),
-    datetime(2019, 5, 29),
-    datetime(2019, 5, 30)
-]
+data = pd.read_csv('data.csv')
+price_date = data['Date']
+price_close = data['Close']
 
-y = [0, 1, 3, 4, 6, 5, 7]
-
-plt.plot_date(dates, y, linestyle='solid')
+plt.plot_date(price_date, price_close, linestyle='solid')
 
 plt.gcf().autofmt_xdate()
 
-date_format = mpl_dates.DateFormatter('%b %d, %Y')
-
-plt.gca().xaxis.set_major_formatter(date_format)
-
-# data = pd.read_csv('data.csv')
-# price_date = data['Date']
-# price_close = data['Close']
-
-# plt.title('Bitcoin Prices')
-# plt.xlabel('Date')
-# plt.ylabel('Closing Price')
+plt.title('Bitcoin Prices')
+plt.xlabel('Date')
+plt.ylabel('Closing Price')
 
 plt.tight_layout()
 
