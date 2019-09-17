1ab8254e9a63f15030b49b2ce0090b311e86cb8c
Phil Lembo <phil.lembo@gmail.com>
2019-09-17 14:03:58 -0400

    Histograms; coloring; custom binds

--- 06-Histograms/starting_code.py
+++ 06-Histograms/starting_code.py
@@ -4,6 +4,10 @@ from matplotlib import pyplot as plt
 plt.style.use('fivethirtyeight')
 
 ages = [18, 19, 21, 25, 26, 26, 30, 32, 38, 45, 55]
+bins = [20, 30, 40, 50, 60]
+
+
+plt.hist(ages, bins=bins, edgecolor='black')
 
 # data = pd.read_csv('data.csv')
 # ids = data['Responder_id']
