2b1f217d2c48b2324f8ef20ee6b6a635ab56e657
Phil Lembo <phil.lembo@gmail.com>
2019-09-17 14:22:48 -0400

    Read data from file, increase bins, plot median age

--- 06-Histograms/starting_code.py
+++ 06-Histograms/starting_code.py
@@ -3,20 +3,21 @@ from matplotlib import pyplot as plt
 
 plt.style.use('fivethirtyeight')
 
-ages = [18, 19, 21, 25, 26, 26, 30, 32, 38, 45, 55]
-bins = [20, 30, 40, 50, 60]
+data = pd.read_csv('data.csv')
+ids = data['Responder_id']
+ages = data['Age']
+
+bins = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 
 
-plt.hist(ages, bins=bins, edgecolor='black')
+plt.hist(ages, bins=bins, edgecolor='black', log=True)
 
-# data = pd.read_csv('data.csv')
-# ids = data['Responder_id']
-# ages = data['Age']
+median_age = 29
+color = '#fc4f30'
 
-# median_age = 29
-# color = '#fc4f30'
+plt.axvline(median_age, color=color, label='Age Median', linewidth=2)
 
-# plt.legend()
+plt.legend()
 
 plt.title('Ages of Respondents')
 plt.xlabel('Ages')
