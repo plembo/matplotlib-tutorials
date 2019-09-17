96e7e9084e10e76a955c931adfa591666f92e47f
Phil Lembo <phil.lembo@gmail.com>
2019-09-17 14:46:15 -0400

    Try out scatterplot options

--- 07-ScatterPlots/starting_code.py
+++ 07-ScatterPlots/starting_code.py
@@ -7,11 +7,17 @@ plt.style.use('seaborn')
 x = [5, 7, 8, 5, 6, 7, 9, 2, 3, 4, 4, 4, 2, 6, 3, 6, 8, 6, 4, 1]
 y = [7, 4, 3, 9, 1, 3, 2, 5, 2, 4, 8, 7, 1, 6, 4, 9, 7, 7, 5, 1]
 
+colors = [7, 5, 9, 7, 5, 7, 2, 5, 3, 7, 1, 2, 8, 1, 9, 2, 5, 6, 7, 5]
 
-# colors = [7, 5, 9, 7, 5, 7, 2, 5, 3, 7, 1, 2, 8, 1, 9, 2, 5, 6, 7, 5]
+sizes = [209, 486, 381, 255, 191, 315, 185, 228, 174,
+        538, 239, 394, 399, 153, 273, 293, 436, 501, 397, 539]
 
-# sizes = [209, 486, 381, 255, 191, 315, 185, 228, 174,
-#          538, 239, 394, 399, 153, 273, 293, 436, 501, 397, 539]
+plt.scatter(x, y, s=sizes, c=colors, cmap='Greens', edgecolor='black', linewidth=1, alpha=0.75)
+
+cbar = plt.colorbar()
+cbar.set_label('Satisfaction')
+
+
 
 # data = pd.read_csv('2019-05-31-data.csv')
 # view_count = data['view_count']
