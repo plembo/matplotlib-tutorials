042b93ecd076374678faaf0740b546b44b48c84c
Phil Lembo <phil.lembo@gmail.com>
2019-09-17 20:44:06 -0400

    Clean up; share x axis

--- 10-Subplots/starting_code.py
+++ 10-Subplots/starting_code.py
@@ -10,7 +10,7 @@ dev_salaries = data['All_Devs']
 py_salaries = data['Python']
 js_salaries = data['JavaScript']
 
-fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1)
+fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, sharex=True)
 
 ax1.plot(ages, dev_salaries, color='#444444',
          linestyle='--', label='All Devs')
@@ -20,11 +20,9 @@ ax2.plot(ages, js_salaries, label='JavaScript')
 
 ax1.legend()
 ax1.set_title('Median Salary (USD) by Age')
-ax1.set_xlabel('Ages')
 ax1.set_ylabel('Median Salary (USD)')
 
 ax2.legend()
-ax2.set_title('Median Salary (USD) by Age')
 ax2.set_xlabel('Ages')
 ax2.set_ylabel('Median Salary (USD)')
 
