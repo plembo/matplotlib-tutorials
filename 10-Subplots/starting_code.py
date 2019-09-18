1862610ba1ab98a26286f17f0d1f65b9112e3c5e
Phil Lembo <phil.lembo@gmail.com>
2019-09-17 20:41:37 -0400

    Split into two axes to show 2 plots

--- 10-Subplots/starting_code.py
+++ 10-Subplots/starting_code.py
@@ -10,19 +10,23 @@ dev_salaries = data['All_Devs']
 py_salaries = data['Python']
 js_salaries = data['JavaScript']
 
-fig, ax = plt.subplots()
+fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1)
 
-ax.plot(ages, py_salaries, label='Python')
-ax.plot(ages, js_salaries, label='JavaScript')
-
-ax.plot(ages, dev_salaries, color='#444444',
+ax1.plot(ages, dev_salaries, color='#444444',
          linestyle='--', label='All Devs')
 
-ax.legend()
+ax2.plot(ages, py_salaries, label='Python')
+ax2.plot(ages, js_salaries, label='JavaScript')
 
-ax.set_title('Median Salary (USD) by Age')
-ax.set_xlabel('Ages')
-ax.set_ylabel('Median Salary (USD)')
+ax1.legend()
+ax1.set_title('Median Salary (USD) by Age')
+ax1.set_xlabel('Ages')
+ax1.set_ylabel('Median Salary (USD)')
+
+ax2.legend()
+ax2.set_title('Median Salary (USD) by Age')
+ax2.set_xlabel('Ages')
+ax2.set_ylabel('Median Salary (USD)')
 
 plt.tight_layout()
 
